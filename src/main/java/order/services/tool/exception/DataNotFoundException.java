121cc59cd8705742f04514a10f72a3b333ff81d4
darwinabelardo-cwt <djaabelardo@gmail.com>
2020-04-07 01:08:35 +0800

    fix formatting

--- src/main/java/order/services/tool/exception/DataNotFoundException.java
+++ src/main/java/order/services/tool/exception/DataNotFoundException.java
@@ -2,7 +2,6 @@ package order.services.tool.exception;
 
 public class DataNotFoundException extends RuntimeException
 {
-
     private static final long serialVersionUID = -1415222240548305646L;
 
     public DataNotFoundException(String message)
